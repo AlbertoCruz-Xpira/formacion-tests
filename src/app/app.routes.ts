20fd550e06dc5e303049ad0f75e0039143582233
Alberto <acrufig@arte-consultores.com>
2026-03-13 14:25:25 +0100

    creating project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { DashboardPage } from './core/pages/dashboard/dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {  path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+    {  path: 'dashboard', component: DashboardPage },
+
+];
