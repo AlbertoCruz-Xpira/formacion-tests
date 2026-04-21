02290c74d01ddaeeaf9652c8a818ff776dcbad40
José Carlos Rodríguez Cortés <jrodcor@kuflow.com>
2026-04-21 11:09:41 +0100

    Fixed problem with relative routes when using the web with a subpath as base url

--- src/app/share/components/header/header.ts
+++ src/app/share/components/header/header.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { MenuItem } from '../menu-item/menu-item';
 
 @Component({
   selector: 'app-header',
-  imports: [MenuItem],
+  imports: [MenuItem, RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
